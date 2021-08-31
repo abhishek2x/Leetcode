f76b411488233b6f0b2118a85e45751ff2d39dd0
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-08-31 23:28:11 +0530

    Automated git commit

--- Medium/15.cpp
+++ Medium/15.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) {
-        vector<vector<int>> res;
+        set<vector<int>>ans;
 		int n = a.size();
 		sort(a.begin(), a.end());
         
@@ -13,13 +13,25 @@ public:
         	int e1 = a[i];
 			int l=i+1;
 			int h = n-1;
-		
+            
         	while(l<h) {
 				int sum = e1 + a[l] + a[h]; 
-				if(sum == 0) cout << e1 << a[l] << a[h] << "\n";
+				if(sum == 0) {
+                    ans.insert({a[i],a[l],a[h]});
+                    l++;
+                }
 				if(sum < 0) l++;
 				else h--;
 			}
 		}
+        vector<vector<int>>finalans;
+        set<vector<int>>::iterator it=ans.begin();
+         
+        while(it!=ans.end()){
+            finalans.push_back(*it);
+            it++;
+        }
+        
+        return finalans;
     }
 };
