10bbf7c0cbb7f4bf2a8b91b4f8d9986ac2384d63
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-09-05 04:13:00 +0530

    Automated git commit

--- Medium/28.cpp
+++ Medium/28.cpp
@@ -1,3 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& a, int k) {
+        vector<int> v;
+
+        int r = a.size();
+        int c = a[0].size();
+
+        for(int i=0; i<r; i++)
+            for(int j=0; j<c; j++)
+                v.push_back(a[i][j]);
+
+        sort(v.begin(), v.end());
+        return v[k-1];
+    }
+};
