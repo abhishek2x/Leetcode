a7694393da051b18e95d3b0fa3c4cfd9d696f84f
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-09-05 03:00:09 +0530

    Automated git commit

--- Medium/26.cpp
+++ Medium/26.cpp
@@ -4,6 +4,20 @@ using namespace std;
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int> v;
         
+        for(int i=0; i<n; i++) {
+            int csum=0;
+            for(int j=i; j<n; j++) {
+                csum += nums[j];
+                v.push_back(csum);
+            }
+        }
+
+        sort(v.begin(), v.end());
+
+        int ans=0;
+        for(int i=left-1; i<=right-1; i++) ans = (ans+v[i])%(1000000007);
+        return ans;
     }
 };
