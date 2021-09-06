b699eb678dff2b639893eb22a0973ea092afef9d
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-09-06 16:18:53 +0530

    Automated git commit

--- Medium/29.cpp
+++ Medium/29.cpp
@@ -1,3 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int, int> mp; //<level, sum> 
+    
+    void do_work(TreeNode* root, int level) {
+        if(!root) return;
+        mp[level] += root->val;
+        do_work(root->left, level+1);
+        do_work(root->right, level+1);
+    }
+    
+    int deepestLeavesSum(TreeNode* root) {
+        mp.clear();
+        do_work(root, 0);
+        return mp.rbegin()->second;
+    }
+};
--- Medium/30.cpp
+++ Medium/30.cpp
@@ -1,3 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:    
+    int sum;
+    int ol;
+    
+    void do_work(TreeNode* root, int cl) {
+        if(!root) return;
+        
+        if(cl == ol) sum += root->val;
+        if(cl > ol) {
+            ol = cl;
+            sum=root->val;
+        }
+        
+        do_work(root->left, cl+1);
+        do_work(root->right, cl+1);
+    }
+    
+    int deepestLeavesSum(TreeNode* root) {
+        sum=0;
+        ol=0;
+        do_work(root, 0);
+        return sum;
+    }
+};
