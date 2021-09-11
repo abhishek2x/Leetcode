eb045b51515d626cabd541a64aac5d9abd840b05
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-09-11 17:53:54 +0530

    Automated git commit

--- Medium/58.cpp
+++ Medium/58.cpp
@@ -17,7 +17,7 @@ public:
         
         if(root->val < low) 
             return trimBST(root->right, low, high);
-        if(root->val > high)
+        if(root->val > high)up
             return trimBST(root->left, low, high);
         
         root->left = trimBST(root->left, low, high);
---
+++ Medium/59.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum;
+    
+    void dowork(TreeNode* root) {
+        if(!root) return;
+        
+        dowork(root->right);
+        root->val += sum;
+        sum = root->val;
+        dowork(root->left);
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        sum=0;
+        dowork(root);
+        return root;
+    }
+};
