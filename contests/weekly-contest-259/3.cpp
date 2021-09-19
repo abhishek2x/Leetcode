5a731a2850010d8bb52be8db921999ab3c7f96e8
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-09-19 08:57:28 +0530

    Automated git commit

--- contests/weekly-contest-259/3.cpp
+++ contests/weekly-contest-259/3.cpp
@@ -9,11 +9,35 @@ public:
     }
     
     void add(vector<int> point) {
-        a[point[0]][point[1]] = 1;
+        a[point[0]][point[1]] += 1;
     }
     
     int count(vector<int> point) {
-        
+        // It doesn't add the new point
+        int row = point[0]; 
+        int col = point[1]; 
+
+        int ans=0;
+
+        // from 0 to col
+        for(int i=0; i<1000; i++) {
+            if(i == col) continue;
+            if(a[row][i]) {
+                int dis = abs(col-i);
+
+                // bottom
+                if(row+dis < 1000) 
+                    if(a[row+dis][i] && a[row+dis][col]) 
+                        ans += max({a[row+dis][i], a[row+dis][col], a[row][i], a[row][col]});
+
+                // top
+                if(row-dis >= 0) 
+                    if(a[row-dis][i] && a[row-dis][col]) 
+                        ans += max({a[row-dis][i], a[row-dis][col], a[row][i], a[row][col]});
+
+            }
+        }
+        return ans;
     }
 };
 
