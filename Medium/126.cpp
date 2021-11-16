8ba7f06eb732f5284754eedcc4786bdb526d9952
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-11-17 00:23:20 +0530

    Automated git commit

---
+++ Medium/126.cpp
@@ -0,0 +1,26 @@
+/*!
+* Copyright (c) 2020 Abhishek Srivastava
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve() {
+
+}
+
+int main(){
+
+    ios :: sync_with_stdio(false);
+    cin.tie(0);
+
+    float c = 35/0;
+
+
+    cout << c ;
+
+    
+
+    return 0;
+}
