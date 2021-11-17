90dd371819c616f58d0487508913a1c96091774a
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-11-17 16:09:37 +0530

    Automated git commit

--- Medium/126.cpp
+++ Medium/126.cpp
@@ -1,26 +1,24 @@
-/*!
-* Copyright (c) 2020 Abhishek Srivastava
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-
-void solve() {
-
-}
-
-int main(){
-
-    ios :: sync_with_stdio(false);
-    cin.tie(0);
-
-    float c = 35/0;
-
-
-    cout << c ;
-
+class Solution {
+public:
+    int dp[101][101];
     
-
-    return 0;
-}
+    int worker(int i, int j, int m, int n) {
+        if(min(i, j) < 0 or i>=m or j>=n) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(i == m-1 && j == n-1) return 1;
+        
+        int ans = 0;
+        ans += worker(i+1, j, m, n);
+        ans += worker(i, j+1, m, n);
+        
+        return dp[i][j] = ans;
+    }
+    
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        return worker(0, 0, n, m);
+    }
+    
+};
