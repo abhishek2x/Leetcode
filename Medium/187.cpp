c5ea40b7ae2c1b9c522ecb022a0af1e648b39561
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2022-06-19 21:56:29 +0530

    Automated git commit

---
+++ Medium/187.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    void _permute(int index, vector<int>& nums, vector<vector<int>>& res) {
+        if(index == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        
+        for(int i=index; i<nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            _permute(index+1, nums, res);
+            swap(nums[index], nums[i]);
+        }
+    }
+        
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        _permute(0, nums, res);
+        return res;
+    }
+};
