1c52b7ca8d6fa24ee1904b059b9a27f4d9a235b3
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2022-07-07 18:16:27 +0530

    Automated git commit

--- Medium/196.cpp
+++ Medium/196.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
-// Assume base address of "GeeksQuiz" to be 1000
-int main()
-{
-   printf(5 + "GeeksQuiz");
-   return 0;
-}
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> res(n);
+        
+        for(int i=nums.size()*2-1; i>=0; i--) {
+            while(!st.empty() and st.top() <= nums[i%n]) st.pop();
+            
+            if(i < n) {
+                if(st.empty()) res[i] = -1;
+                else res[i] = st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        
+        return res;
+    }
+};
